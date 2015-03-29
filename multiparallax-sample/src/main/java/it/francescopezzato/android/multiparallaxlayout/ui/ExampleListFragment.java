522497308218c5c386e7ce0319fd2e9e9566607c
francesco pezzato <fpezzato@gmail.com>
2015-03-29 16:01:35 +0100

    Added fragment layout explicitly to have full control, no longer  a ListFragment

--- multiparallax-sample/src/main/java/it/francescopezzato/android/multiparallaxlayout/ui/ExampleListFragment.java
+++ multiparallax-sample/src/main/java/it/francescopezzato/android/multiparallaxlayout/ui/ExampleListFragment.java
@@ -1,9 +1,16 @@
 package it.francescopezzato.android.multiparallaxlayout.ui;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AbsListView;
+import android.widget.ListView;
 
 import it.francescopezzato.android.MultiparallaxLayout;
 import it.francescopezzato.android.multiparallaxlayout.R;
@@ -15,11 +22,13 @@ import static rx.android.app.AppObservable.bindFragment;
 /**
  * Created by francesco on 22/03/2015.
  */
-public class ExampleListFragment extends ListFragment {
+public class ExampleListFragment extends Fragment {
 
+	ListView mListView;
 	ListAdapter mAdapter;
 
 	MultiparallaxLayout mHeader;
+	Drawable mToolbarBackground;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -27,16 +36,27 @@ public class ExampleListFragment extends ListFragment {
 	}
 
 	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_list, container, false);
+		mListView = (ListView) view.findViewById(android.R.id.list);
+		return view;
+	}
+
+	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
+
+		mToolbarBackground = new ColorDrawable(Color.RED);
+		((ActionBarActivity) getActivity()).getSupportActionBar().setBackgroundDrawable(mToolbarBackground);
+
 		mAdapter = new ListAdapter(getActivity());
 
-		mHeader = (MultiparallaxLayout) getActivity().getLayoutInflater().inflate(R.layout.list_header, getListView(), false);
-		getListView().addHeaderView(mHeader);
+		mHeader = (MultiparallaxLayout) getActivity().getLayoutInflater().inflate(R.layout.list_header, mListView, false);
+		mListView.addHeaderView(mHeader);
 
-		setListAdapter(mAdapter);
+		mListView.setAdapter(mAdapter);
 
-		getListView().setOnScrollListener(new AbsListView.OnScrollListener() {
+		mListView.setOnScrollListener(new AbsListView.OnScrollListener() {
 			@Override
 			public void onScrollStateChanged(AbsListView view, int scrollState) {
 
@@ -45,14 +65,25 @@ public class ExampleListFragment extends ListFragment {
 			@Override
 			public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
 				if (firstVisibleItem == 0) {
-					View firstChildView = getListView().getChildAt(0);
+					View firstChildView = mListView.getChildAt(0);
 					if (firstChildView != null) {
-						int scrollY = -firstChildView.getTop();
+						float scrollY = -firstChildView.getTop();
 						if (mHeader != null) {
-							mHeader.setOffsetY(scrollY);
+							mHeader.setOffsetY((int)scrollY);
+							if (firstChildView.getTop() < 0) {
+								int actionBarHeight = 0;
+								if (getActivity() instanceof ActionBarActivity) {
+									actionBarHeight = ((ActionBarActivity) getActivity()).getSupportActionBar().getHeight();
+								}
+								float ratiod4 = clamp(scrollY / (mHeader.getHeight() - actionBarHeight), 0f, 1f);
+								mToolbarBackground.setAlpha((int) (ratiod4 * 255));
+							}
 						}
 					}
+
+
 				}
+
 
 			}
 		});
@@ -63,8 +94,11 @@ public class ExampleListFragment extends ListFragment {
 		super.onResume();
 		Observable<String> demoData = new DataGenerator().getDemoData();
 		mAdapter.replace(bindFragment(this, demoData));
-		setListShown(true);
+
 	}
 
+	private static float clamp(float val, float min, float max) {
+		return Math.max(min, Math.min(max, val));
+	}
 
 }
--- multiparallax-sample/src/main/java/it/francescopezzato/android/multiparallaxlayout/ui/MainActivity.java
+++ multiparallax-sample/src/main/java/it/francescopezzato/android/multiparallaxlayout/ui/MainActivity.java
@@ -2,6 +2,7 @@ package it.francescopezzato.android.multiparallaxlayout.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -14,7 +15,10 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		if (toolbar != null) {
+			setSupportActionBar(toolbar);
+		}
 		getSupportFragmentManager()
 			.beginTransaction().replace(R.id.content_container,new ExampleListFragment()).commit();
 	}
